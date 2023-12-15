d8d1bf6dcb965a0450e61628709b7a8e3575d56c
kennyAnyi9 <kennyanyi9@gmail.com>
2023-12-15 06:50:27 +0000

    apply motion effects  to overlay text on hero section

--- app/components/slide-show.tsx
+++ app/components/slide-show.tsx
@@ -8,6 +8,7 @@ import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import { slideImages } from "@/data/data";
 import { Button } from "../lib/exports";
+import Motion from "../framer";
 
 const slideProperties = {
   duration: 15000,
@@ -15,6 +16,10 @@ const slideProperties = {
   arrows: false,
 };
 
+const childVariants = {
+  hidden: { opacity: 0, y: -20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+};
 
 const Slide = () => {
   return (
@@ -30,10 +35,11 @@ const Slide = () => {
             />
 
             <div className="absolute inset-0 bg-black bg-opacity-60"></div>
-            <div className="absolute w-full h-screen flex gap-10 lg:mt-10 justify-center items-center flex-col  mx-auto ">
-              <div className="flex flex-col gap-3 w-11/12 lg:w-3/5">
-                <span className=" text-3xl lg:text-6xl text-white font-montserrat font-normal text-center leading-tight ">
-                  {/* <Typed
+            <Motion childVariants={childVariants}>
+              <div className="absolute w-full h-screen flex gap-10 lg:mt-10 justify-center items-center flex-col  mx-auto ">
+                <div className="flex flex-col gap-3 w-11/12 lg:w-3/5">
+                  <span className=" text-3xl lg:text-6xl text-white font-montserrat font-normal text-center leading-tight ">
+                    {/* <Typed
                     strings={[
                       `${slideImage.caption1}${slideImage.caption2} ${slideImage.caption3}\n${slideImage.caption4}\n${slideImage.caption5}`,
                     ]}
@@ -41,20 +47,21 @@ const Slide = () => {
                     backSpeed={0}
                     loop
                   /> */}
-                  {slideImage.caption1}
-                </span>
+                    {slideImage.caption1}
+                  </span>
+                </div>
+                <div className="flex flex-row gap-5 lg:gap-10 ">
+                  <Button
+                    text="learn more"
+                    className="bg-transparent border border-orange-600  "
+                  />
+                  <Button
+                    text="About us"
+                    className="border border-orange-600 bg-orange-600 "
+                  />
+                </div>
               </div>
-              <div className="flex flex-row gap-5 lg:gap-10 ">
-                <Button
-                  text="learn more"
-                  className="bg-transparent border border-orange-600  "
-                />
-                <Button
-                  text="About us"
-                  className="border border-orange-600 bg-orange-600 "
-                />
-              </div>
-            </div>
+            </Motion>
           </section>
         ))}
       </Fade>
