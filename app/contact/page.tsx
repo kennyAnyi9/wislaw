871059610fa77b20f61e7b91c7fd2f5e108be1f9
kennyAnyi9 <kennyanyi9@gmail.com>
2023-12-24 06:04:07 +0000

    feat: add contact form

---
+++ app/contact/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ContactForm } from "../lib/exports";
+import Image from "next/image";
+
+const Page = () => {
+  return (
+    <div className="flex min-h-screen max-w-screen justify-center items-center flex-row relative  ">
+      <div className="hidden lg:block relative w-1/2 min-h-screen top-20 ">
+        <Image
+          alt="contact-image"
+          fill
+          className="w-full h-full object-cover "
+          src="/images/dark.jpg"
+        />
+      </div>
+      <div className="flex-1 flex justify-center items-center ">
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
