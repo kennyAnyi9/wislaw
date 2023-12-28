a3d4f0912be1422d5894ee6caae1cf82b520f9c8
kennyAnyi9 <kennyanyi9@gmail.com>
2023-12-28 18:20:18 +0000

    chore: replace large video with compressed one

--- app/components/about-us/text-video.tsx
+++ app/components/about-us/text-video.tsx
@@ -3,26 +3,10 @@
 import { video } from "@/app/lib/exports";
 import React from "react";
 
-import { motion } from "framer-motion";
-import useScrollAnimation from "../hooks/use-video-toogle";
-
 const Video: React.FC = () => {
-  // Use the custom hook to get the ref and the inView value
-  const { ref, inView } = useScrollAnimation();
-
   return (
     <div className="video__section top-20 justify-center items-center">
-      <motion.video
-        ref={ref}
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: inView ? 1 : 0, scale: inView ? 1 : 0 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-        src="/images/dadwen.mp4"
-        loop
-        autoPlay
-        muted
-        className="w-full"
-      ></motion.video>
+      <video src="/dadwen.mp4" loop autoPlay muted className="w-full"></video>
 
       <div className="video__text ">
         <h1 className={`${video.className} video__title text-center`}>
