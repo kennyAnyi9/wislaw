fe74e53e37150738ecbb8625f3b0a89f1ba6023a
kennyAnyi9 <kennyanyi@gmail.com>
2024-05-13 03:58:14 +0000

    set up news page

---
+++ app/news/page.tsx
@@ -0,0 +1,10 @@
+import { TextGenerateEffectDemo } from "@/components/about-us/hero-text";
+import React from "react";
+
+export default function News() {
+  return (
+    <div>
+      <TextGenerateEffectDemo />
+    </div>
+  );
+}
