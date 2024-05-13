b2ccea79ff9a0bfbb012480ba5c43be0765c2413
kennyAnyi9 <kennyanyi@gmail.com>
2024-05-13 03:54:16 +0000

    configure stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
-const svgToDataUri = require("mini-svg-data-uri");
+import themer from "@tailus/themer";
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
-
-import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
@@ -31,7 +29,18 @@ const config = {
         sans: ["Geist", "Inter", ...defaultTheme.fontFamily.sans],
         mono: ["GeistMono", "fira-code", ...defaultTheme.fontFamily.mono],
       },
-      colors: {
+      colors: ({ colors }) => ({
+        secondary: colors.lime,
+        accent: colors.yellow,
+        success: colors.lime,
+        danger: colors.red,
+        warning: colors.yellow,
+        info: colors.sky,
+        gray: colors.slate,
+        white: colors.white,
+        black: colors.black,
+        transparent: colors.transparent,
+        // themer stuff ends here
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -41,10 +50,10 @@ const config = {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
+        // secondary: {
+        //   DEFAULT: "hsl(var(--secondary))",
+        //   foreground: "hsl(var(--secondary-foreground))",
+        // },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
@@ -53,10 +62,10 @@ const config = {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
         },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
+        // accent: {
+        //   DEFAULT: "hsl(var(--accent))",
+        //   foreground: "hsl(var(--accent-foreground))",
+        // },
         popover: {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))",
@@ -65,7 +74,8 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-      },
+      }),
+
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -77,16 +87,6 @@ const config = {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
-        spotlight: {
-          "0%": {
-            opacity: "0",
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: "1",
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -95,11 +95,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        loop: {
+          to: {
+            "offset-distance": "100%",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        spotlight: "spotlight 2s ease .75s 1 forwards",
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
@@ -107,28 +111,17 @@ const config = {
   },
   plugins: [
     addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          "bg-grid": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-grid-small": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-dot": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`,
-          }),
+    themer({
+      radius: "smooth",
+      background: "light",
+      border: "light",
+      padding: "large",
+      components: {
+        button: {
+          rounded: "lg",
         },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      );
-    },
+      },
+    }),
     require("tailwindcss-animate"),
   ],
 } satisfies Config;
